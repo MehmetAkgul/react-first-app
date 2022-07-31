54e06c9205a3c50ba074e012d72f463b4931e74d
mehmet <mehmetakgul.dev@gmail.com>
2022-07-31 13:14:35 +0300

    butten text

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+
+    const buttonText = 'Hello World';
+
+
+    return (
+        <div>
+            <label htmlFor="name" className="label"> enter e-mail</label>
+            <input type="text" id="name"/>
+            <button style={{backgroundColor: 'red', color: 'white'}}>
+                {buttonText}
+            </button>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector("#root")
+)
